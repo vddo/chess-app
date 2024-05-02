bc0c68af2aa53d0c014fba061effee832768dcc4
Viet Duc Do <duc.do32@mailbox.org>
2024-05-02 21:52:04 +1200

    pawn movement and avail moves

--- lab-lichess/CGames.py
+++ lab-lichess/CGames.py
@@ -7,15 +7,19 @@ class CGame:
         self.time_control = 'blitz'
         self.turn_count = 0
         self.board_history = []
-        self.players_turn = 'white'
+        self.players_turn = 'w'
 
 
 class Board:
     def __init__(self):
-        self.players_turn = 'white'
+        self.players_turn = 'w'
         self.estimation = 0.0
         self.all_available_moves = []
         self.position = []
+
+
+    def valid_move(self, move: list[int, int]) -> bool:
+        """Takes move from piece and checks if the move is valid in this position"""
 
 
 class Piece:
@@ -25,23 +29,6 @@ class Piece:
         self.available_moves = []
 
 
-    # [i, j] <- [ranks, lines]
-    def move_1_up(self, start: list[int, int]):
-        i, j = start
-        if self.color == 'w':
-            return (i+1, j)
-
-        return (i-1, j)
-
-    # def move_x_up(self, start: list[int, int], x: int):
-        
-
-            
-
-        
-        
-
-class Pawn(Piece):
     def init_color(self, col):
         if col in ('w', 'b'):
             self.color = col
@@ -53,6 +40,34 @@ class Pawn(Piece):
         self.pos_coordinates = coord
 
 
+    # [i, j] <- [ranks, lines]
+    def move_1_up(self, start: list[int, int]):
+        i, j = start
+        if self.color == 'w':
+            return (i+1, j)
+
+        return (i-1, j)
+
+    def move_x_up(self, start: list[int, int], x: int):
+        i, j = start
+        if self.color == 'w':
+            return (i+x, j)
+
+        return (i-x, j)
+        
+
+
+class Pawn(Piece):
+
     def init_avail_moves(self):
-        self.available_moves = []
+        if self.pos_coordinates is not None:
+            self.available_moves = []
+            tmp_moves = []
+            tmp_moves.append(self.move_x_up(self.pos_coordinates, 1))
+            tmp_moves.append(self.move_x_up(self.pos_coordinates, 2))
+            self.available_moves = tmp_moves
+        else:
+            raise ValueError('Position must be initialized first.')
+
+
 
---
+++ lab-lichess/test_pawn_class.py
@@ -0,0 +1,55 @@
+import CGames
+import pytest
+
+# In chess you say (line, rank) like a2
+# coordinates are implemented [i,j] <- (rank, line)
+coord_a2 = [2, 1]
+coord_e7 = [7, 5]
+
+# Initialize pawn
+pawn_w = CGames.Pawn()
+pawn_w.init_color('w')
+pawn_w.init_pos_coord(coord_a2)
+
+pawn_b = CGames.Pawn()
+pawn_b.init_color('b')
+pawn_b.init_pos_coord(coord_e7)
+
+pawn_error = CGames.Pawn()
+
+
+def f_move_pawn_w_up():
+    return (pawn_w.move_1_up(coord_a2), pawn_w.move_x_up(coord_a2, 2))
+
+def f_move_pawn_b_up():
+    return (pawn_b.move_1_up(coord_e7), pawn_b.move_x_up(coord_e7, 2))
+
+def f_pawn_color_error():
+    return pawn_error.init_color('red')
+
+def f_pawn_w_available_moves():
+    pawn_w.init_avail_moves()
+    return pawn_w.available_moves
+
+def f_pawn_b_available_moves():
+    pawn_b.init_avail_moves()
+    return pawn_b.available_moves
+
+
+def test_move_up():
+    # Assert pawn movement
+    assert f_move_pawn_w_up() == ((3, 1), (4, 1))
+    assert f_move_pawn_b_up() == ((6, 5), (5, 5))
+
+    # Color error 
+    with pytest.raises(ValueError):
+        f_pawn_color_error()
+
+
+    # Available pawn moves
+    assert f_pawn_w_available_moves() == [(3, 1), (4, 1)]
+    assert f_pawn_b_available_moves() == [(6, 5), (5, 5)]
+
+    # init moves before init position
+    with pytest.raises(ValueError):
+        pawn_error.init_avail_moves()
