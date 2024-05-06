b4e153da6e98057349d943635c21825cf374cf88
Viet Duc Do <duc.do32@mailbox.org>
2024-05-06 17:45:42 +1200

    change current pos to tuple

--- lab-lichess/test_board_class.py
+++ lab-lichess/test_board_class.py
@@ -3,6 +3,14 @@ from CGames import Board
 from icecream import ic
 import pytest
 
+def generate_homesquares():
+    s = set()
+    rows = [1, 2, 7, 8]
+    for i in rows:
+        for j in range(8):
+            s.add(( i, j ))
+
+    return s
 
 # Init board
 b = Board()
@@ -15,3 +23,24 @@ def b_init_piece():
 
 def test_init():
     assert b_init_piece() == ('King king1')
+
+# Test init board
+def test_init_board():
+    b.init_board()
+    l_pieces = list(b.active_pieces.keys())
+    assert len(l_pieces) == 18
+
+    s = generate_homesquares()
+    for elem in l_pieces:
+        s.remove(b.active_pieces[elem].current_square)
+    assert len(l_pieces) == 0
+
+
+
+
+if __name__ == '__main__':
+    s = generate_homesquares()
+    print(s)
+    b.init_board()
+    l_pieces = list(b.active_pieces.keys())
+    print(l_pieces)
